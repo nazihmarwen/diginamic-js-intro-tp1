510f5153b462081f85c98e03d869e6fe0b4724a3
NAZIH Marwane <nazih.marwane1993@gmail.com>
2019-10-15 16:20:33 +0200

    Commit initial

--- exercice-1.js
+++ exercice-1.js
@@ -2,16 +2,15 @@ function fibonacci(nbr) {
    var n1 = 0;
    var n2 = 1;
    var somme = 0;
-    document.write(n1 + "<br/>");
-    document.write(n2 +"<br/>");
-   for(let i = 2; i <= nbr; i++){
+   console.log(n1,n2);
+   for(let i = 2; i <= 20; i++){
       //somme des deux derniers nombres
       somme = n1 + n2; 
       //assigner la dernière valeur à la première
       n1 = n2; 
       //attribuer la somme au dernier
       n2 = somme; 
-       document.write(somme+"<br/>");
+    console.log(somme);
    }
  
 }
